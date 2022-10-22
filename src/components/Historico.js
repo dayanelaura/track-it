f221a784db31c6f51bc3ec24b54cda5e96cfc257
Dayane <dayanelaura3@gmail.com>
2022-10-22 20:30:17 -0300

    Tela histórico implementada

--- src/components/Historico.js
+++ src/components/Historico.js
@@ -1,8 +1,35 @@
-export default function Historico(){
-    
-    return (
-        <div>
-            Tela histórico
-        </div>
-    )
+import styled from "styled-components";
+import Rodape from "./Rodape";
+import Topo from "./Topo";
+
+export default function Historico() {
+  return (
+    <>
+      <Topo />
+      <HistoricoContainer>
+        <h1>Histórico</h1>
+        <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+      </HistoricoContainer>
+      <Rodape />
+    </>
+  );
 }
+
+const HistoricoContainer = styled.div`
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    padding: 110px 10px 0 10px;
+    background-color: #E5E5E5;
+
+    h1{
+        color: #126BA5;
+        font-size: 23px;
+        margin-bottom:18px;
+    }
+
+    p{
+        color: #666666;
+        font-size: 18px;
+    }
+`
